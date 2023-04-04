71198f64316abb2c5fc30881e48da7da88890e33
Anil Maharjan <anilmaharjan@mentorfriends.com>
2023-04-04 13:37:24 +0545

    add: login session storage and the data when update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let authToken = ''
 
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    setStorage()
     const loginBtn = document.querySelector('.btn-login')
     loginBtn.innerText = 'Logging...'
     loginBtn.classList.add('disabled')
@@ -33,6 +34,7 @@ loginForm.addEventListener('submit', (e) => {
     .then(data => {
         // console.log('login', data)
         authToken = data.data.token
+        sessionStorage.setItem("token", authToken);
         // console.log('authToken', authToken)
 
         loginForm.reset()
@@ -41,6 +43,21 @@ loginForm.addEventListener('submit', (e) => {
         getSuggestionBox()
     })
 })
+
+const setStorage = () => {
+    sessionStorage.setItem("isLoggedIn", "true");
+}
+
+onload = (event) => {
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn')
+    // console.log(isLoggedIn)
+    if (isLoggedIn) {
+        welcomeSection.classList.add('d-none')
+        todoSection.classList.remove('d-none')
+        authToken = sessionStorage.getItem('token')
+    }
+    getSuggestionBox()
+};
 
 const getSuggestionBox = async () => {
     await fetch(`https://apitest.boomconcole.com/api/list-api-clean?type=suggestionBox`)
@@ -83,7 +100,6 @@ const getSuggestionBox = async () => {
                             <input type="checkbox" name="isTodoCompleted" id="isTodoCompleted" value="${isTodoCompleted}" ${isChecked}>
                             <label for="isTodoCompleted"></label>
                         </div>
-
                     </div>
                     `
                 document.querySelector('.todo-list').innerHTML += todoItem
@@ -125,14 +141,12 @@ const updateTodo = (data) => {
     // console.log('id', checkboxInfo.id)
 
     let checkboxData = {
-        "data": {
-            "suggestionBox": {
-                "suggestion": checkboxInfo.data.suggestionBox.suggestion,
-                "url": checkboxInfo.data.suggestionBox.url,
-                "isTodoCompleted": checkboxInfo.data.suggestionBox.isTodoCompleted
-            },
+        "suggestionBox": {
+            "suggestion": checkboxInfo.data.suggestionBox.suggestion,
+            "url": checkboxInfo.data.suggestionBox.url,
+            "isTodoCompleted": checkboxInfo.data.suggestionBox.isTodoCompleted,
             "id": checkboxInfo.id
-        },
+        }
     }
 
     if (checkboxInfo.data.suggestionBox.image_path) {
